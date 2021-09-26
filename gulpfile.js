4a05e431a4501d385b99df9e562797d0898c4e39
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2021-09-26 10:57:44 +0800

    Change Gulp Src and Dest from /build to /_site

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 const {src, dest} = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const minifyInline = require('gulp-minify-inline');
-const postcss = require('gulp-html-postcss');
 const through2 = require('through2');
 
 const AmpOptimizer = require('@ampproject/toolbox-optimizer');
 const ampOptimizer = AmpOptimizer.create();
 
 function build(cb) {
-  return src('./build/**/*.html')
+  return src('./_site/**/*.html')
     .pipe(
       through2.obj(async (file, _, cb) => {
         if (file.isBuffer()) {
@@ -22,7 +21,7 @@ function build(cb) {
     )
     .pipe(htmlmin({ collapseWhitespace: false }))
     .pipe(minifyInline())
-    .pipe(dest('./build/'));
+    .pipe(dest('./_site/'));
 }
 
 exports.default = build;
