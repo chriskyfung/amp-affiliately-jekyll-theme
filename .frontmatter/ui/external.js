3aae089784808db2e3f20b4b364197a9e255b573
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-11-24 11:54:28 +0800

    🔧 chore(config): add external.js for custom card image
    - Created new file `.frontmatter/ui/external.js`:
      - Imported `registerCardImage` and `enableDevelopmentMode` from Front Matter extensibility.
      - Registered a custom card image rendering function.
    
    - Updated `frontmatter.json`:
      - Added `frontMatter.extensibility.scripts` entry to include the new external script.
    
    These changes enable custom card image rendering and development mode for enhanced functionality in the Front Matter CMS.

---
+++ .frontmatter/ui/external.js
@@ -0,0 +1,12 @@
+import { registerCardImage, enableDevelopmentMode } from "https://cdn.jsdelivr.net/npm/@frontmatter/extensibility/+esm";
+// enableDevelopmentMode();
+
+/**
+ * @param {string} filePath - The path of the file
+ * @param {object} data - The metadata of the file
+ * @returns {string} - The HTML to be rendered in the card footer
+ */
+registerCardImage(async (filePath, metadata) => {
+    const image = metadata.fmPreviewImage ? metadata.fmPreviewImage : metadata.image.path ? metadata.fmWebviewUrl + metadata.image.path : `${metadata.fmWebviewUrl}/assets/images/write-2160925.svg`;
+    return `<img src="${image}" alt="${metadata.title}" style="object-fit: cover; margin: auto auto" class="h-36" />`;
+});
