fa83c80805433971baaff44b55fe2e2b3390deee
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-11-27 19:42:52 +0800

    🐛 fix(ui): enhance image rendering logic in external.js for card images
    - **Image Rendering Logic:**
      - Improved the conditionals to correctly handle different scenarios for rendering card images.
      - Adjusted the logic to account for cases where `metadata.image` might be available directly or nested within `metadata.image.path`.
    
    These changes ensure that the correct image is displayed for card images based on the available metadata, enhancing the user interface's robustness.

--- .frontmatter/ui/external.js
+++ .frontmatter/ui/external.js
@@ -7,6 +7,9 @@ import { registerCardImage, enableDevelopmentMode } from "https://cdn.jsdelivr.n
  * @returns {string} - The HTML to be rendered in the card footer
  */
 registerCardImage(async (filePath, metadata) => {
-    const image = metadata.fmPreviewImage ? metadata.fmPreviewImage : metadata.image.path ? metadata.fmWebviewUrl + metadata.image.path : `${metadata.fmWebviewUrl}/assets/images/write-2160925.svg`;
+    const image = metadata.fmPreviewImage ?
+        metadata.fmPreviewImage : metadata.image ?
+            metadata.fmWebviewUrl + metadata.image : metadata.image.path ?
+                metadata.fmWebviewUrl + metadata.image.path : `${metadata.fmWebviewUrl}/assets/images/write-2160925.svg`;
     return `<img src="${image}" alt="${metadata.title}" style="object-fit: cover; margin: auto auto" class="h-36" />`;
 });
