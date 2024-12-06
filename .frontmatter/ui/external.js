75de09640b8282b7d5d4cf7e7eb0b91f37ab5fff
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2024-12-06 10:04:35 +0800

    🐛 fix: correct image registration logic in external.js
    Improved the image registration logic to handle different metadata properties correctly.
    - Adjusted the order of conditions to prioritize the `image.path` property over the `image` property.
    
    Files affected:
    - `.frontmatter/ui/external.js`

--- .frontmatter/ui/external.js
+++ .frontmatter/ui/external.js
@@ -8,8 +8,8 @@ import { registerCardImage, enableDevelopmentMode } from "https://cdn.jsdelivr.n
  */
 registerCardImage(async (filePath, metadata) => {
     const image = metadata.fmPreviewImage ?
-        metadata.fmPreviewImage : metadata.image ?
-            metadata.fmWebviewUrl + metadata.image : metadata.image.path ?
-                metadata.fmWebviewUrl + metadata.image.path : `${metadata.fmWebviewUrl}/assets/images/write-2160925.svg`;
+        metadata.fmPreviewImage : metadata.image.path ?
+            metadata.fmWebviewUrl + metadata.image.path : metadata.image ?
+                metadata.fmWebviewUrl + metadata.image : `${metadata.fmWebviewUrl}/assets/images/write-2160925.svg`;
     return `<img src="${image}" alt="${metadata.title}" style="object-fit: cover; margin: auto auto" class="h-36" />`;
 });
